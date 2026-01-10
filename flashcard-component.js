53aedb55447fccb310fb9a3780fedbaccf29de3f
WojtowiczZuzanna <zuzia.wojtowicz2004@gmail.com>
2026-01-10 18:25:32 +0100

    Animacja

--- flashcard-component.js
+++ flashcard-component.js
@@ -1,5 +1,50 @@
 const template = document.createElement('template');
 template.innerHTML = `
+<style>
+    :host {
+      display: block;
+      width: 100%;
+      height: 100%;
+      perspective: 1200px;
+    }
+
+    .card {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      transform-style: preserve-3d;
+      transition: transform 0.6s ease;
+      cursor: pointer;
+    }
+
+    .card.flipped {
+      transform: rotateY(180deg);
+    }
+
+    .front, .back {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      backface-visibility: hidden;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 1.3rem;
+      border-radius: 10px;
+      padding: 10px;
+      background: #aedfffff;
+    }
+
+    .back {
+      background: #65bffbff;
+      transform: rotateY(180deg);
+    }
+
+    .card, .front, .back {
+    box-sizing: border-box;
+    }
+  </style>
+
   <div class="card">
     <div class="front"></div>
     <div class="back"></div>
